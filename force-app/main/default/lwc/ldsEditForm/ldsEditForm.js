a6b7d74e7a47838fa156896df66c6f6ff8e327a3
Eneshh07 <eneshorazova21@gmail.com>
2022-01-17 23:15:06 +0500

    LDS added

---
+++ force-app/main/default/lwc/ldsEditForm/ldsEditForm.js
@@ -0,0 +1,29 @@
+import { LightningElement, api } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import LEAD_SOURCE_FIELD from '@salesforce/schema/Contact.LeadSource';
+import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
+import BIRTHDATE_FIELD from '@salesforce/schema/Contact.Birthdate';
+import MAILING_ADDRESS_FIELD from '@salesforce/schema/Contact.MailingAddress';
+
+
+export default class LdsEditForm extends LightningElement {
+    @api recordId;
+    @api objectApiName;
+
+    nameField = NAME_FIELD;
+    phoneField = PHONE_FIELD;
+    leadSourceField = LEAD_SOURCE_FIELD;
+    birthdateField = BIRTHDATE_FIELD;
+    mailingAddress = MAILING_ADDRESS_FIELD;
+
+
+    handleSubmit(){
+        alert('The rocord is about to submitted');
+    }
+
+handleSuccess(){
+    alert('LDS  the record has been created');
+}
+
+
+}
---
+++ force-app/main/default/lwc/ldsViewForm/ldsViewForm.js
@@ -0,0 +1,7 @@
+import { LightningElement,api } from 'lwc';
+
+export default class LdsViewForm extends LightningElement {
+
+    @api recordId;
+    @api objectApiName;
+}
