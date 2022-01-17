f30dd32e9671fdb3fb689d7c97a62f8ffc22fb57
Eneshh07 <eneshorazova21@gmail.com>
2022-01-18 01:49:39 +0500

    lds wire get added

---
+++ force-app/main/default/lwc/ldsWireGet/ldsWireGet.js
@@ -0,0 +1,54 @@
+import { LightningElement , wire } from 'lwc';
+import { getObjectInfo , getObjectInfos , getPicklistValues } from 'lightning/uiObjectInfoApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import STATE_FIELD from '@salesforce/schema/Account.State__c';
+import COUNTRY_FIELD from '@salesforce/schema/Account.Country__c';
+import CITY_FIELD from '@salesforce/schema/Account.City__c';
+
+export default class LdsWireGet extends LightningElement {
+    @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
+    getObjectInfoData({ data, error }) {
+        if (data) {
+            console.log('getObjectInfo=>Account Info: ', JSON.stringify(data));
+        } else if (error) {
+            console.log('Error getObjectInfo: ', JSON.stringify(error));
+        }
+    }
+
+    @wire(getObjectInfos, { objectApiNames: [CONTACT_OBJECT, ACCOUNT_OBJECT] })
+    getObjectInfosData({ data, error }){
+        if (data) {
+            console.log('getObjectInfos=>: ', JSON.stringify(data));
+        } else if (error) {
+            console.log('Error getObjectInfos: ', JSON.stringify(error));
+        }
+    }
+
+    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: COUNTRY_FIELD })
+    getCountryPicklist({ data, error }) {
+        if (data) {
+            console.log('Country Picklist=>: ', JSON.stringify(data));
+        } else if (error) {
+            console.log('Error State: ', JSON.stringify(error));
+        }
+    }
+
+    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: STATE_FIELD })
+    getStatePicklist({ data, error }) {
+        if (data) {
+            console.log('State Picklist=>: ', JSON.stringify(data));
+        } else if (error) {
+            console.log('Error State: ', JSON.stringify(error));
+        }
+    }
+    
+    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: CITY_FIELD })
+    getCityPicklist({ data, error }) {
+        if (data) {
+            console.log('City Picklist=>: ', JSON.stringify(data));
+        } else if (error) {
+            console.log('Error State: ', JSON.stringify(error));
+        }
+    }
+}
